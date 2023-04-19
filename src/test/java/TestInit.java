e47488794917b211e2da609118c56a1da203a9a5
Martin SEBIROT <96467585+MarreTeint@users.noreply.github.com>
2023-04-19 09:19:22 +0200

    [TP Noté] début

--- src/test/java/TestInit.java
+++ src/test/java/TestInit.java
@@ -1,25 +1,5 @@
-import org.junit.rules.ExpectedException;
-import testEtat.*;
 import org.junit.*;
-import static org.junit.Assert.*;
 
 public class TestInit {
-
-	private Conteneur C;
-
-	@Test
-	public void testTransition() throws ErreurConteneur {
-		C = new Conteneur(5);
-		if(!C.estVide()){
-			fail("Le conteneur n'est pas vide à l'initialisation");
-		}
-		if(C.capacite() != 5){
-			fail("La capacité du conteneur n'est pas correcte");
-		}
-	}
-	@Test(expected=ErreurConteneur.class)
-	public void testTransitionVide() throws ErreurConteneur {
-		C = new Conteneur(-5);
-	}
 
 }
