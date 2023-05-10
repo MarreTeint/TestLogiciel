2cbe2d4e17051a62f977f78d1dd9fb782e03c478
kevin <death-joke>
2023-05-10 10:55:00 +0200

    [TP Noté] ajout couverture de test sur System

--- src/main/java/ErreurSysteme.java
+++ src/main/java/ErreurSysteme.java
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-
-
->>>>>>> d612a7c ([TP Noté] ajout couverture de test sur System)
 public class ErreurSysteme extends Exception {
     public ErreurSysteme(String msg) { super(msg); }
 }
